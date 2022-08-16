b09fc8c4f87807c90a7a0f4d244d8b1e27ae4d13
qeesig <109809041+qeesig@users.noreply.github.com>
2022-08-16 05:44:01 +0000

    Deploying to gh-pages from @ qeesig/blanko@ca475bba3bdbe5a62ece9dbf0f3e525d6c562c72 🚀

---
+++ _next/static/chunks/ae51ba48-23999312e3604070.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[260],{8010:function(a,b,c){c.d(b,{bjh:function(){return e}});var d=c(5177);function e(a){return(0,d.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M289.94 256l95-95A24 24 0 00351 127l-95 95-95-95a24 24 0 00-34 34l95 95-95 95a24 24 0 1034 34l95-95 95 95a24 24 0 0034-34z"}}]})(a)}}}])
