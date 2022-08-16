ccb6c5789419d33b5125e5bfe10f9f2332c88860
qeesig <qisigonline@gmail.com>
2022-08-16 20:17:56 +0800

    add svgr

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   basePath: "/blanko",
   // assetPrefix: "/blanko/",
   trailingSlash: true,
