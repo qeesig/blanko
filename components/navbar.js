406a05f2ab4fe289c7fc1d7a8ca9c2e3d37972ab
qeesig <qisigonline@gmail.com>
2022-08-17 00:56:05 +0800

    add logo link

--- components/navbar.js
+++ components/navbar.js
@@ -55,11 +55,12 @@ function DrawerAppBar(props) {
       <Typography
         variant="h6"
         sx={{
-          padding: "14px 18px 12px 18px",
+          padding: "10px 18px 8px 18px",
           backgroundColor: "#1976d2",
           display: "grid",
-          gridTemplateColumns: "40px 1fr",
+          gridTemplateColumns: "40px 126px",
           columnGap: "20px",
+          height: "56px",
         }}
       >
         <IconButton
@@ -68,15 +69,22 @@ function DrawerAppBar(props) {
           edge="start"
           onClick={handleDrawerToggle}
           sx={{
-            padding: "4px 0",
             marginLeft: 0,
+            marginTop: "2px",
             border: "2px solid rgba(255,255,255, .15)",
             borderRadius: "6px",
+            height: "32px",
           }}
         >
           <CloseIcon />
         </IconButton>
-        <Logo style={{ width: "126px", height: "26px", marginTop: "2px" }} />
+        <Link href="/">
+          <a>
+            <Logo
+              style={{ width: "126px", height: "26px", marginTop: ".39rem" }}
+            />
+          </a>
+        </Link>
       </Typography>
       <Divider sx={{ borderColor: "transparent" }} />
       <List>
@@ -145,11 +153,18 @@ function DrawerAppBar(props) {
             >
               <MenuIcon />
             </IconButton>
-            <Logo
-              style={{ width: "126px", height: "26px", marginTop: "2px" }}
-            />
+            <Link href="/">
+              <a>
+                <Logo
+                  style={{
+                    width: "126px",
+                    height: "26px",
+                    marginTop: ".19rem",
+                  }}
+                />
+              </a>
+            </Link>
           </div>
-
           <Box
             sx={{
               display: { xs: "none", sm: "block" },
@@ -164,7 +179,6 @@ function DrawerAppBar(props) {
                     color: "#fff",
                     fontSize: "14px",
                     textTransform: "capitalize",
-                    paddingTop: "10px",
                   }}
                 >
                   Blankos
@@ -179,7 +193,6 @@ function DrawerAppBar(props) {
                     color: "#fff",
                     fontSize: "14px",
                     textTransform: "capitalize",
-                    paddingTop: "10px",
                   }}
                 >
                   Accessories
