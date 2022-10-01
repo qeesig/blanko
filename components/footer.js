864d57c7724f761b6ac4da40cfd77e21b56474b1
qeesig <qisigonline@gmail.com>
2022-10-02 00:16:06 +0800

    update footer

--- components/footer.js
+++ components/footer.js
@@ -4,17 +4,12 @@ import styles from "../styles/footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Created by{" "}
-      <Link
-        href="https://twitter.com/qeesig"
-        target="_blank"
-        sx={{
-          color: "white",
-          textDecoration: "underline",
-        }}
-      >
-        @qeesig
-      </Link>
+      <div>
+        Created by{" "}
+        <Link href="https://twitter.com/qeesig" target="_blank">
+          @qeesig
+        </Link>
+      </div>
     </footer>
   );
 }
