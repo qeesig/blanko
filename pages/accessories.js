73af2f168c4bb412ffb86c28b66126a76207626e
qeesig <qisigonline@gmail.com>
2022-10-10 07:28:59 +0800

    update title

--- pages/accessories.js
+++ pages/accessories.js
@@ -359,7 +359,7 @@ export default function Accessories() {
   return (
     <div className={styles.pageContainer}>
       <Head>
-        <title>Accessories - BlankoDrops</title>
+        <title>Accessories</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           property="twitter:title"
--- pages/index.js
+++ pages/index.js
@@ -362,7 +362,7 @@ export default function Blankos() {
   return (
     <div className={styles.pageContainer}>
       <Head>
-        <title>Blankos - BlankoDrops</title>
+        <title>Blankos</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           property="twitter:title"
