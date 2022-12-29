44298395b591640d09e1a14b64471e090742a441
qeesig <qisigonline@gmail.com>
2022-12-29 08:51:15 +0800

    add data

---
+++ database/data.js
@@ -0,0 +1,24 @@
+export const partyPass = [
+  { label: "S00 First Party Pass" },
+  { label: "S00 Hustle & Glow" },
+  { label: "S00 Grave Rave" },
+  { label: "S00 Manila Chill" },
+  { label: "S00 Get Lucky" },
+  { label: "S00 Blanko Brawlers" },
+  { label: "S00 Summer Camp Slam" },
+  { label: "S00 Snack Attack" },
+  { label: "S01 IKWYD Last Grave Rave" },
+  { label: "S01 Cosmic Groove" },
+];
+
+export const seasons = [
+  { label: "Season 0" },
+  { label: "S00 Pre-Alpha" },
+  { label: "S00 Alpha" },
+  { label: "S00 Private Beta" },
+  { label: "S00 Open Beta" },
+  { label: "S00 Early Access" },
+  { label: "Season 1" },
+];
+
+export const lastUpdated = "2022-12-28";
