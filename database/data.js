b64d7d706b94a57092cd82c561f472f16bb6a276
qeesig <qisigonline@gmail.com>
2023-01-14 11:44:05 +0800

    update db

--- database/data.js
+++ database/data.js
@@ -21,5 +21,5 @@ export const seasons = [
   { label: "Season 1" },
 ];
 
-export const lastUpdated = new Date(Date.UTC(2023, 0, 13, 0, 0, 0, 0));
+export const lastUpdated = new Date(Date.UTC(2023, 0, 14, 0, 0, 0, 0));
 // console.log(lastUpdated);
